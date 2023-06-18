88b98e94b866896613471dd2422a35d0b5b9e8b4
suman <sumanpok9@gmail.com>
2023-06-18 23:12:44 +0545

    remove mongodb for postgresql

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import express, { Application, Request, Response } from "express";
-import mongoose from "mongoose";
+import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
 const app: Application = express();
 // const dotenv = require("dotenv").config();
-import dotenv from "dotenv";
-import connectDatabase from "./config/db";
+import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
-connectDatabase();
+app.use(express.json());
 
 const PORT = 9000;
-app.get("/", (req: Request, res: Response) => {
-  console.log("Root Directory");
-  res.send("Hello browiser");
+app.get('/', (req: Request, res: Response) => {
+  console.log('Root Directory');
+  res.send('Hello browiser');
 });
 
-app.listen(PORT, () => console.log("Server Running on " + PORT));
+app.listen(PORT, () => console.log('Server Running on ' + PORT));
